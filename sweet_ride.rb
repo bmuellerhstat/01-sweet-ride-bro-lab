07c8c4b170a8c32584628b2da2bcf4777ad1b4cc
Brandon Mueller <brianmuellermusic@gmail.com>
2016-10-03 14:42:23 +0000

    3 out of 5 passing

--- sweet_ride.rb
+++ sweet_ride.rb
@@ -1,1 +1,12 @@
 #code your answers here
+def check_it_out
+	'Check out my sweet new ride, dude!'
+end
+
+def no_big_deal(car)
+	"No big deal, it's a #{car}"
+end
+
+def stolen
+	"dude, where's my car?"
+end
